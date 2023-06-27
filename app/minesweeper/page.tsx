9e0e084326c12048b1d0e6355ecf0a96fdad881b
JustoRago <103001263+asdt560@users.noreply.github.com>
2023-06-27 14:50:02 -0300

    Add mines

--- app/minesweeper/page.tsx
+++ app/minesweeper/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 function Cell (status : string) {
   
   return (
-    <div className="cell"></div>
+    <div className={status}></div>
   )
 }
 export default function MineSweeper() {
@@ -23,12 +23,44 @@ export default function MineSweeper() {
       const row = []
       for(let j = 0; j < boardSize; j++) {
         const element = Cell(TILE_STATUSES.HIDDEN)
-        row.push({ element, i, j })
+        let tile = { 
+          element,
+          i,
+          j,
+          mine: minePositions.some(p => p.i === i && p.j === j),
+          get status () {
+            return element.props.className;
+          },
+          set status (value) {
+            element.props.className = value;
+          }
+        }
+        row.push(tile)
       }
       board.push(row)
     }
+    console.log(board)
     return board;
   }
+  const getMinePositions = (boardSize : number, minesNumber : number) => {
+    interface position {
+      i: number,
+      j: number
+    }
+    const minePositions : position[] = []
+    while(minePositions.length < minesNumber) {
+      const position = {
+        i: Math.floor(Math.random() * boardSize),
+        j: Math.floor(Math.random() * boardSize)
+      }
+      if(!minePositions.some(p => p.i === position.i && p.j === position.j)) {
+        minePositions.push(position)
+      }
+    }
+    return minePositions;
+  }
+  const minePositions = getMinePositions(boardSize!, minesNumber!)
+  console.log(minePositions)
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setBoardSize(((e.target as HTMLFormElement).elements['0'] as HTMLFormElement).valueAsNumber)
@@ -58,15 +90,15 @@ export default function MineSweeper() {
       <div 
         className={boardSize ? `w-fit grid bg-gray-600` : "board"}
         style={{
-          gridTemplateColumns : boardSize ? `repeat(${boardSize}, 60px)` : undefined,
-          gridTemplateRows : boardSize ? `repeat(${boardSize}, 60px)` : undefined,
+          gridTemplateColumns : boardSize ? `repeat(${boardSize}, 45px)` : undefined,
+          gridTemplateRows : boardSize ? `repeat(${boardSize}, 45px)` : undefined,
         }}
       >
         {boardSize && minesNumber ?
           createBoard(boardSize, minesNumber).map((row, i) => {
                 return (row.map((cell, j) => {
                   return (
-                    <div className="justify-self-center self-center h-14 w-14 bg-slate-500 border-white border-2" key={j}>
+                    <div className="justify-self-center self-center h-10 w-10 bg-slate-500 border-white border-2" key={j}>
                       {cell.element}
                     </div>
                   )
