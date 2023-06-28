7c783d2e80e6c1a0a03fae0110f9f4af8bd141d9
JustoRago <103001263+asdt560@users.noreply.github.com>
2023-06-28 17:12:41 -0300

    Build minesweeper

--- app/minesweeper/page.tsx
+++ app/minesweeper/page.tsx
@@ -1,73 +1,24 @@
 'use client';
 import React from "react";
+import Board from "./Board";
 
-function Cell (status : string) {
-  
-  return (
-    <div className={status}></div>
-  )
-}
 export default function MineSweeper() {
   const [boardSize, setBoardSize] = React.useState<number | null>(null);
   const [minesNumber, setMinesNumber] = React.useState<number | null>(null);
-
-  const createBoard = (boardSize : number, minesNumber : number) => {
-    const board = []
-    const TILE_STATUSES = {
-      HIDDEN: 'hidden',
-      MINE: 'mine',
-      NUMBER: 'number',
-      MARKED: 'marked',
-    }
-    for(let i = 0; i < boardSize; i++) {
-      const row = []
-      for(let j = 0; j < boardSize; j++) {
-        const element = Cell(TILE_STATUSES.HIDDEN)
-        let tile = { 
-          element,
-          i,
-          j,
-          mine: minePositions.some(p => p.i === i && p.j === j),
-          get status () {
-            return element.props.className;
-          },
-          set status (value) {
-            element.props.className = value;
-          }
-        }
-        row.push(tile)
-      }
-      board.push(row)
-    }
-    console.log(board)
-    return board;
-  }
-  const getMinePositions = (boardSize : number, minesNumber : number) => {
-    interface position {
-      i: number,
-      j: number
-    }
-    const minePositions : position[] = []
-    while(minePositions.length < minesNumber) {
-      const position = {
-        i: Math.floor(Math.random() * boardSize),
-        j: Math.floor(Math.random() * boardSize)
-      }
-      if(!minePositions.some(p => p.i === position.i && p.j === position.j)) {
-        minePositions.push(position)
-      }
-    }
-    return minePositions;
-  }
-  const minePositions = getMinePositions(boardSize!, minesNumber!)
-  console.log(minePositions)
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if(boardSize && minesNumber) {
+      resetBoard();
+    }
     setBoardSize(((e.target as HTMLFormElement).elements['0'] as HTMLFormElement).valueAsNumber)
     setMinesNumber(((e.target as HTMLFormElement).elements['1'] as HTMLFormElement).valueAsNumber)
   }
+  const resetBoard = () => {
+    setBoardSize(null);
+    setMinesNumber(null);
+  }
   return (
-    <div>
+    <div className="game">
       <h3 className="title">Minesweeper</h3>
       <form onSubmit={(e) => handleSubmit(e)}>
         <input
@@ -84,28 +35,12 @@ export default function MineSweeper() {
         />
         <button type="submit">Start Game</button>
       </form>
-      <div className="subtext">
-        Mines Left: {minesNumber}
-      </div>
-      <div 
-        className={boardSize ? `w-fit grid bg-gray-600` : "board"}
-        style={{
-          gridTemplateColumns : boardSize ? `repeat(${boardSize}, 45px)` : undefined,
-          gridTemplateRows : boardSize ? `repeat(${boardSize}, 45px)` : undefined,
-        }}
-      >
-        {boardSize && minesNumber ?
-          createBoard(boardSize, minesNumber).map((row, i) => {
-                return (row.map((cell, j) => {
-                  return (
-                    <div className="justify-self-center self-center h-10 w-10 bg-slate-500 border-white border-2" key={j}>
-                      {cell.element}
-                    </div>
-                  )
-                }))
-          })
-        : null}
-      </div>
+      {
+        boardSize && minesNumber ?
+        <Board size={boardSize!} mines={minesNumber!} />
+        : null
+      }
+      <button type="button" onClick={() => resetBoard()}>Reset Game</button>
     </div>
   )
 }
