71cec39eaee26f41f413d3d23e00ba60c5d3d089
JustoRago <103001263+asdt560@users.noreply.github.com>
2023-07-06 17:22:07 -0300

    Set up game components

--- app/stuck/Piece.tsx
+++ app/stuck/Piece.tsx
@@ -1,23 +1,45 @@
-
+'use client';
+import { useRef, useState } from 'react';
 interface PieceProps {
-    form: string
+    children: React.ReactNode,
+    position: {
+      x: {
+        start: number,
+        end: number
+      }, y: {
+        start: number,
+        end: number
+      }
+    }
 }
 
 const Piece = (props : PieceProps) => {
-  let pieceShape = "span 1/span 1";
-  if(props.form === "col") {
-    pieceShape = "span 2/span 1";
-  } else if(props.form === "bar") {
-    pieceShape = "span 1/span 2";
-  }
-    
+  const [position, setPosition] = useState(props.position);
+  const pieceRef = useRef(null);
+
+  const handleDrag = (e : MouseEvent) => {
+    setPosition({
+      x: {
+        start: Math.min(Math.max(0, position.x.start + Math.floor(e.movementX/70)), 5),
+        end: Math.min(Math.max(0, position.x.end + Math.floor(e.movementX/70)), 5)
+      },
+      y: {
+        start: Math.min(Math.max(0, position.y.start + Math.floor(e.movementY/70)), 5),
+        end: Math.min(Math.max(0, position.y.end + Math.floor(e.movementY/70)), 5)
+      }
+    });
+  };
+
     return (
         <div 
-          className="bg-green-500 rounded-md border-2 border-green-700"
+          className="bg-green-500 rounded-md border-2 border-green-700 z-30"
           style={{
-            gridArea: pieceShape,
+            gridColumn: `${position.x.start}/${position.x.end}`,
+            gridRow: `${position.y.start}/${position.y.end}`,
           }}
+          ref={pieceRef}
         >
+          {position.x.start},{position.x.end},{position.y.start},{position.y.end}
         </div>
     )
 }
--- app/stuck/StuckBase.tsx
+++ app/stuck/StuckBase.tsx
@@ -1,21 +1,45 @@
+'use client';
 import Piece from './Piece'
 import Target from './Target'
 
 const StuckBase = () => {
+  const startPositions = {
+    zero: { x: { start: 1, end: 1 }, y: { start: 1, end: 3 } },
+    one: { x: { start: 4, end: 4 }, y: { start: 1, end: 3 } },
+    two: { x: { start: 1, end: 1 }, y: { start: 3, end: 5 } },
+    three: { x: { start: 2, end: 4 }, y: { start: 3, end: 3 } },
+    four: { x: { start: 4, end: 4 }, y: { start: 3, end: 5 } },
+    five: { x: { start: 1, end: 1 }, y: { start: 5, end: 5 } },
+    six: { x: { start: 4, end: 4 }, y: { start: 5, end: 5 } },
+    seven: { x: { start: 2, end: 2 }, y: { start: 4, end: 4 } },
+    eight: { x: { start: 3, end: 3 }, y: { start: 4, end: 4 } },
+  }
   return (
     <div className="flex flex-col">
-      <div className="grid grid-cols-4 grid-rows-5 h-96 w-80 bg-yellow-400 border-double border-8 border-yellow-500 rounded-xl">
-        <Target></Target>
-        <Piece form="col"></Piece>
-        <Piece form="col"></Piece>
-        <Piece form="col"></Piece>
-        <Piece form="col"></Piece>
-        <Piece form="bar"></Piece>
-        <Piece form="square"></Piece>
-        <Piece form="square"></Piece>
-        <Piece form="square"></Piece>
-        <Piece form="square"></Piece>
+      <div className='h-96 relative w-80 bg-yellow-400 border-double border-8 border-yellow-500 rounded-xl z-0'>
+        <div className="grid h-full w-full grid-cols-4 grid-rows-5 z-40">
+          <Piece position={startPositions.zero}>0</Piece>
+          <Target></Target>
+          <Piece position={startPositions.one}>1</Piece>
+          <Piece position={startPositions.two}>2</Piece>
+          <Piece position={startPositions.three}>3</Piece>
+          <Piece position={startPositions.four}>4</Piece>
+          <Piece position={startPositions.five}>5</Piece>
+          <Piece position={startPositions.six}>6</Piece>
+          <Piece position={startPositions.seven}>7</Piece>
+          <Piece position={startPositions.eight}>8</Piece>
+        </div>
+        <div className='z-10 absolute flex flex-col items-center justify-end' style={{
+          background: 'repeating-linear-gradient(45deg, red, red 10px, transparent, transparent 20px)',
+          width: '152px',
+          height: '147.2px',
+          top: '222px',
+          left:'75.5px'
+        }}>
+          <p className="text-red-600 font-black bg-yellow-500 border-2 border-red-600 p-0.5 rounded-md">EXIT</p>
+        </div>
       </div>
+
       <button type="button">Reset</button>
     </div>
   )
