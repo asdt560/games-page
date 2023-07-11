82ca10d6ed13850e3f70a660cebd9a4cdf94a31a
JustoRago <103001263+asdt560@users.noreply.github.com>
2023-07-11 17:01:06 -0300

    Create link to snake page

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Games</h1>
       <Link href="/minesweeper">Minesweeper</Link>
+      <Link href="/snake">Snake</Link>
     </main>
   )
 }
