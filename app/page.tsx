275addf9808c847a1ef7a719023df87474deda38
JustoRago <103001263+asdt560@users.noreply.github.com>
2023-07-19 18:18:33 -0300

    Alter main page style

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,15 @@ import Image from 'next/image'
 import Link from 'next/link';
 
 export default function Home() {
+  const linkClass = "border-4 rounded-md border-slate-500 hover:bg-slate-500 hover:text-white transition-colors duration-300 ease-in-out"
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24">
       <h1>Games</h1>
-      <Link href="/minesweeper">Minesweeper</Link>
-      <Link href="/snake">Snake</Link>
-      <Link href="/hangman">Hangman</Link>
+      <div className="flex">
+        <Link className={linkClass} href="/minesweeper">Minesweeper</Link>
+        <Link className={linkClass} href="/snake">Snake</Link>
+        <Link className={linkClass} href="/hangman">Hangman</Link>
+      </div>
     </main>
   )
 }
